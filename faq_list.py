d2c4b266a5af65ba9cc3363f415920347a54f742
Ocawesome101 <50844998+Ocawesome101@users.noreply.github.com>
2020-04-10 14:14:29 -0400

    Update faq_list.py

--- faq_list.py
+++ faq_list.py
@@ -5,5 +5,6 @@ faqs = [
     ("(servers?)|(multiplayer)", "ComputerCraft MultiPlayer Servers", "servers.md"),
     ("((turtle)? ?(re)?fuel(ing)?)|((turtle.)?getFuelLevel)|((turtle.)?refuel)", "Turtle fuel", "turtle_fuel.md"),
     ("paste(bin)?", "Pastebin", "pastebin.md"),
-    ("center(ing)? ?(text)?", "Centering text", "center_write.md")
+    ("center(ing)? ?(text)?", "Centering text", "center_write.md"),
+    ("(don((')t)( )ask?", "Don't ask to ask", "dontask.md")
 ]
