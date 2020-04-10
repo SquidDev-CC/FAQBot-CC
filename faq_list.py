4bdd2f2ea1b0841a6368865ff0d1e6005d4acea8
Wendelstein7 <23213464+Wendelstein7@users.noreply.github.com>
2020-04-10 20:30:32 +0200

    fixed don't ask regex

--- faq_list.py
+++ faq_list.py
@@ -6,5 +6,5 @@ faqs = [
     ("((turtle)? ?(re)?fuel(ing)?)|((turtle.)?getFuelLevel)|((turtle.)?refuel)", "Turtle fuel", "turtle_fuel.md"),
     ("paste(bin)?", "Pastebin", "pastebin.md"),
     ("center(ing)? ?(text)?", "Centering text", "center_write.md"),
-    ("(don(?(')?t)?( )?ask", "Don't ask to ask", "dontask.md")
+    ("(don('?t) ?)?ask", "Don't ask to ask", "dontask.md")
 ]
