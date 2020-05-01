604fd31d2a66831f6fc0c0d437c43f5a45907aae
JackMacWindows <jackmacwindowslinux@gmail.com>
2020-05-01 19:58:57 -0400

    Update faq_list.py

--- faq_list.py
+++ faq_list.py
@@ -4,8 +4,9 @@ FAQS = [
     # ("example", "Example FAQ", "example.md"),
     ("(servers?)|(multiplayer)", "ComputerCraft MultiPlayer Servers", "servers.md"),
     ("((turtle)? ?(re)?fuel(ing)?)|((turtle.)?getFuelLevel)|((turtle.)?refuel)", "Turtle fuel", "turtle_fuel.md"),
-    ("paste(bin)?", "Pastebin", "pastebin.md"),
+    ("paste(bin)?$", "Pastebin", "pastebin.md"),
     ("center(ing)? ?(text)?", "Centering text", "center_write.md"),
     ("(don('?t) ?)?ask", "Don't ask to ask", "dontask.md"),
-    ("paste(bin)?[-_]((pre-?1.8)|(1.7)|(old([-_]version)?))", "Pastebin on old versions of ComputerCraft", "pastebin-pre1.8.md")
+    ("paste(bin)?[-_ ]((pre\-?1\.8)|(1\.7)|(old([-_ ]version)?))", "Pastebin on old versions of ComputerCraft", "pastebin-pre1.8.md"),
+    ("^(make\-an\-)?os", "Tips on making an OS for ComputerCraft", "make-an-os.md")
 ]
