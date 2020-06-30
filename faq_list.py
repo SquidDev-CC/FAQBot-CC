4ca101b830f6395ec58133d356ea593cc6f1eb9a
Wendelstein7 <23213464+Wendelstein7@users.noreply.github.com>
2020-06-30 20:34:59 +0200

    Flipped regex: the search and the searched

--- faq_list.py
+++ faq_list.py
@@ -2,10 +2,10 @@
 
 FAQS = [
     # ("example", "Example FAQ", "example.md"),
-    ("(servers?)|(multiplayer)", "ComputerCraft MultiPlayer Servers", "servers.md"),
-    ("((turtle)? ?(re)?fuel(ing)?)|((turtle.)?getFuelLevel)|((turtle.)?refuel)", "Turtle fuel", "turtle_fuel.md"),
-    ("paste(bin)?", "Pastebin", "pastebin.md"),
-    ("center(ing)? ?(text)?", "Centering text", "center_write.md"),
-    ("(don('?t) ?)?ask", "Don't ask to ask", "dontask.md"),
-    ("paste(bin)?[-_]((pre-?1.8)|(1.7)|(old([-_]version)?))", "Pastebin on old versions of ComputerCraft", "pastebin-pre1.8.md")
+    ("multiplayer Servers", "ComputerCraft MultiPlayer Servers", "servers.md"),
+    ("turtle refueling getFuelLevel", "Turtle fuel", "turtle_fuel.md"),
+    ("pastebin", "Pastebin", "pastebin.md"),
+    ("centering text", "Centering text", "center_write.md"),
+    ("don't ask", "Don't ask to ask", "dontask.md"),
+    ("old pastebin pre1.8", "Pastebin on old versions of ComputerCraft", "pastebin-pre1.8.md")
 ]
