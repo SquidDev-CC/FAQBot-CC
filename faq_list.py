47c47d80502f0e80886dffd1b31ff9a8ff66fa1a
JackMacWindows <jackmacwindowslinux@gmail.com>
2021-01-16 09:11:06 -0500

    Add FAQ about local IP access (#24)

--- faq_list.py
+++ faq_list.py
@@ -2,13 +2,14 @@
 
 FAQS = [
     # ("example", "Example FAQ", "example.md"),
-    ("multiplayer Servers", "ComputerCraft MultiPlayer Servers", "servers.md"),
+    ("multiplayer servers", "ComputerCraft MultiPlayer Servers", "servers.md"),
     ("turtle refueling getFuelLevel", "Turtle fuel", "turtle_fuel.md"),
     ("pastebin", "Pastebin", "pastebin.md"),
     ("centering text", "Centering text", "center_write.md"),
-    ("don't ask", "Don't ask to ask", "dontask.md"),
+    ("don't ask to ask", "Don't ask to ask", "dontask.md"),
     ("old pastebin pre1.8", "Pastebin on old versions of ComputerCraft", "pastebin-pre1.8.md"),
     ("passing or assigning functions", "Passing or assigning functions", "passing_or_assigning_functions.md"),
     ("error too long without yielding", "Too long without yielding", "too_long_without_yielding.md"),
     ("monitor rendering", "Monitor rendering issues", "monitor_rendering.md"),
+    ("local ips", "Accessing local IPs", "local_ips.md")
 ]
